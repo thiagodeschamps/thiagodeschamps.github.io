795d54f04468b62a1fc0d784b42208f6ae4c8115
root <root@dellthiago.>
2025-07-06 22:12:35 -0300

    Fix GitHub Pages deployment configuration

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/thiagodeschamps.github.io/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 }) 
